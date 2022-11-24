7115b7a934e83d9fc9039a7fe1219c731b6e5cd1
Laura Valentina Ceballos Aguilar <55591683+lauraaac@users.noreply.github.com>
2022-11-24 10:04:12 -0500

    Update progfun/tutoriales/Haskell/Haskell Language_files/tryhaskell.pages.js
    Co-authored-by: jubustamantem <75868064+jubustamantem@users.noreply.github.com>

--- progfun/tutoriales/Haskell/Haskell Language_files/tryhaskell.pages.js
+++ progfun/tutoriales/Haskell/Haskell Language_files/tryhaskell.pages.js
@@ -426,7 +426,7 @@ tryhaskell.pages.list =
 
                     "<p>Me gustaría que convirtieras<code>toUpper</code> mi nombre en mayúscula, por favor.</p> " +
                     +"<p> Me llamo <code>\"Haskellito\"</code>.</p> " +
-                    "<p> Ahora hazlo! Creo en ti, sé el primero en resolverlo. Te conviene...</p>" 
+                    "<p> ¡Ahora hazlo! Creo en ti, sé el primero en resolverlo. Te conviene...</p>" 
 
                     // '<p>Spoiler: <code class="spoiler">map toUpper "Chris"</code></p>'
             },
